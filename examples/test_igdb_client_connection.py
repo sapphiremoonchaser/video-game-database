dca364ab052e26558a9e9bf25ff37ce323b71dd2
violetdiamonds <violetdiamonds@protonmail.com>
2026-08-28 15:34:55 -0500

    example for using class to connect to api

--- examples/test_igdb_client_connection.py
+++ examples/test_igdb_client_connection.py
@@ -1,67 +1,19 @@
-import os
+from xmlrpc import client
 
-import requests
-from dotenv import load_dotenv
-
-load_dotenv()
-
-client_id = os.getenv("IGDB_CLIENT_ID")
-client_secret = os.getenv("IGDB_CLIENT_SECRET")
+from src.video_game_database.api.igdb_client import IgdbClient
 
 
-# Get Twitch access token
-token_response = requests.post(
-    "https://id.twitch.tv/oauth2/token",
-    params={
-        "client_id": client_id,
-        "client_secret": client_secret,
-        "grant_type": "client_credentials",
-    },
-)
+client = IgdbClient()
 
-token_response.raise_for_status()
+query = """
+fields
+    name,
+    first_release_date,
+    rating;
+limit 10;
+"""
 
-access_token = token_response.json()["access_token"]
+games = client.query("games", query)
 
-print("Authentication successful!")
-
-
-# Query IGDB
-headers = {
-    "Client-ID": client_id,
-    "Authorization": f"Bearer {access_token}",
-}
-
-limit = 10
-offset = 0
-
-all_games = []
-
-for page in range(n):
-    query = f"""
-    fields
-        name,
-        first_release_date,
-        rating;
-    limit {limit};
-    offset {offset};
-    """
-
-    response = requests.post(
-        "https://api.igdb.com/v4/games/",
-        headers=headers,
-        data=query,
-    )
-
-    response.raise_for_status()
-
-    games = response.json()
-
-    all_games.extend(games)
-
-    offset += limit
-
-print(f"Retrieved {len(all_games)} games")
-
-for game in all_games:
+for game in games:
     print(game["name"])
--- src/video_game_database/api/igdb_client.py
+++ src/video_game_database/api/igdb_client.py
@@ -22,4 +22,44 @@ class IgdbClient:
         if not self.client_secret:
             raise ValueError("IGDB_CLIENT_SECRET environment variable not set")
 
+        self.access_token = self._get_access_token()
 
+        self.headers = {
+            "Client-ID": self.client_id,
+            "Authorization": f"Bearer {self.access_token}"
+        }
+
+
+    def _get_access_token(self) -> str:
+        """Get an access token from Twitch."""
+
+        response = requests.post(
+            self.TOKEN_URL,
+            params={
+                "client_id": self.client_id,
+                "client_secret": self.client_secret,
+                "grant_type": "client_credentials",
+            },
+        )
+
+        response.raise_for_status()
+
+        return response.json()["access_token"]
+
+
+    def query(
+        self,
+        endpoint: str,
+        query: str
+    ) -> list[dict]:
+        """Send a query to an IGDB endpoint."""
+
+        response = requests.post(
+            f"{self.API_URL}/{endpoint}",
+            headers=self.headers,
+            data=query
+        )
+
+        response.raise_for_status()
+
+        return response.json()
